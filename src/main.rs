18f2b4a9e7a80a8eb235b7dda1805abbe94fb931
Erik Hollensbe <git@hollensbe.org>
2023-03-29 04:56:26 -0700

    add a logger middleware
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,19 @@ use tracing_subscriber::FmtSubscriber;
 static REACT_APP: Dir = include_dir!("react_app/build");
 const INDEX_FILE: &str = "index.html";
 
+async fn log_serve(
+    req: Request<Body>,
+    _resp: Option<Response<Body>>,
+    _params: Params,
+    _app: App<(), NoState>,
+    _state: NoState,
+) -> HTTPResult<NoState> {
+    let path = req.uri().path();
+    info!("Requesting: {}", path);
+
+    Ok((req, None, NoState {}))
+}
+
 async fn serve(
     req: Request<Body>,
     _resp: Option<Response<Body>>,
@@ -23,8 +36,6 @@ async fn serve(
         None => REACT_APP.get_file(INDEX_FILE).unwrap(),
     };
 
-    info!("Requesting path: {}", path);
-
     let body = Body::from(file.contents_utf8().unwrap());
     Ok((
         req,
@@ -36,12 +47,12 @@ async fn serve(
 #[tokio::main]
 async fn main() -> Result<(), ServerError> {
     let subscriber = FmtSubscriber::builder()
-        .with_max_level(Level::TRACE)
+        .with_max_level(Level::INFO)
         .finish();
 
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
     let mut app = App::new();
-    app.get("/*", compose_handler!(serve)).unwrap();
+    app.get("/*", compose_handler!(log_serve, serve)).unwrap();
     app.serve("0.0.0.0:3000").await
 }
