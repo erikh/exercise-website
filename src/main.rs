49e3e36b8a452907bff57eafb3b73a800b6c136d
Erik Hollensbe <git@hollensbe.org>
2023-10-26 09:49:18 -0700

    prepping for a trip to starbucks
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,40 @@
+use anyhow::Result;
 use davisjr::prelude::*;
 use include_dir::{include_dir, Dir};
+use sqlx::SqlitePool;
 use tracing::{debug, info, Level};
 use tracing_subscriber::FmtSubscriber;
 
 static REACT_APP: Dir = include_dir!("react_app/build");
 const INDEX_FILE: &str = "index.html";
+const DB_FILENAME: &str = "exercise.db";
+
+#[derive(Debug, Clone, Default)]
+#[allow(dead_code)]
+struct AppState {
+    db: Option<SqlitePool>,
+}
+
+impl AppState {
+    async fn new(db: &str) -> Result<Self> {
+        // FIXME create db if necessary
+        Ok(Self {
+            db: Some(SqlitePool::connect(&format!("sqlite://{}", db)).await?),
+        })
+    }
+}
+
+impl TransientState for AppState {
+    fn initial() -> Self {
+        Default::default()
+    }
+}
 
 async fn log_serve(
     req: Request<Body>,
     _resp: Option<Response<Body>>,
     _params: Params,
-    _app: App<(), NoState>,
+    _app: App<AppState, NoState>,
     _state: NoState,
 ) -> HTTPResult<NoState> {
     let path = req.uri().path();
@@ -23,7 +47,7 @@ async fn serve(
     req: Request<Body>,
     _resp: Option<Response<Body>>,
     params: Params,
-    _app: App<(), NoState>,
+    _app: App<AppState, NoState>,
     _state: NoState,
 ) -> HTTPResult<NoState> {
     let path = match params.get("*") {
@@ -54,7 +78,10 @@ async fn main() -> Result<(), ServerError> {
 
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
-    let mut app = App::new();
+    let state = AppState::new(DB_FILENAME)
+        .await
+        .map_err(|x| ServerError::from(format!("{}", x)))?;
+    let mut app = App::with_state(state);
     app.get("/*", compose_handler!(log_serve, serve)).unwrap();
     app.serve("0.0.0.0:3000").await
 }
