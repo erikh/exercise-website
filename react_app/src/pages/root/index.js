aaa88405991ce7637a7abef258f079c5aea276f1
Erik Hollensbe <git@hollensbe.org>
2023-10-29 15:53:14 -0700

    lots of polish on form input
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- react_app/src/pages/root/index.js
+++ react_app/src/pages/root/index.js
@@ -11,25 +11,62 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
+import Alert from "@mui/material/Alert";
 import ReorderIcon from "@mui/icons-material/Reorder";
 import AddIcon from "@mui/icons-material/Add";
 
-async function submitExercise() {
+async function submitExercise(setState) {
   let form = document.getElementById("enter_exercise");
   for (var i = 0; i < form.length; i++) {
     if (form.elements[i].name === "name") {
-      await fetch("/input/exercise", {
-        method: "POST",
-        body: JSON.stringify({ name: form.elements[i].value }),
-      });
+      try {
+        let response = await fetch("/input/exercise", {
+          method: "POST",
+          body: JSON.stringify({ name: form.elements[i].value }),
+        });
+
+        if (response.status !== 200) {
+          // FIXME figure out reading the body later
+          setState({ error: "There was an error in the form" });
+        }
+      } catch (error) {
+        setState({ error: error.message });
+      }
     }
   }
 }
 
 export default function Root() {
-  const [state, setState] = React.useState({ open: true });
+  const [state, setState] = React.useState({
+    open: true,
+    error: null,
+    error_timeout: null,
+  });
+
+  let now = new Date();
+
+  if (state["error"]) {
+    if (state["error_timeout"]) {
+      if (now.getTime() > state["error_timeout"]) {
+        setState({ error: null, error_timeout: null });
+      }
+    } else {
+      setState({
+        error: state["error"],
+        error_timeout: now.getTime() + 300000,
+      });
+    }
+  }
+
+  let error = state["error"] ? (
+    <Alert severity="error">{state["error"]}</Alert>
+  ) : (
+    <React.Fragment />
+  );
+
   return (
     <React.Fragment>
+      {error}
       <IconButton>
         <ReorderIcon
           style={{ visibility: !state["open"] ? "visible" : "hidden" }}
@@ -68,7 +105,7 @@ export default function Root() {
                 <Input name="name" size="30em" />
               </Grid>
               <Grid item xs={12}>
-                <Button onClick={() => submitExercise()}>Submit</Button>
+                <Button onClick={() => submitExercise(setState)}>Submit</Button>
               </Grid>
             </Grid>
           </form>
