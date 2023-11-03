07a96dc303ce87c98655fe2c90874b29305869cb
Erik Hollensbe <git@hollensbe.org>
2023-11-03 12:54:55 -0700

    some code that's definitely going to explode the first time I use it
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- react_app/src/lib/utils/index.js
+++ react_app/src/lib/utils/index.js
@@ -1,3 +1,81 @@
 export function assignProperties(stateTemplate, props) {
   return Object.assign({}, stateTemplate, props);
 }
+
+// this somewhat unwieldy function simplifies the process of submitting data
+// via POST and through window.fetch, and using the result to prime react's
+// state management system.
+//
+// - name is the name of the form you're reading
+// - route is the path to the POST endpoint
+// - stateTemplate is a starter set of state which will be merged by
+//   assignProperties.
+// - addlProps is an additional set of static properties to merge with state
+//   after the request has been completed.
+// - setState is react's state modification function passed in for use by this
+//   call.
+// - transformFunc is a custom function that takes a form element and starter
+//   object. It returns an object which replaces the existing object passed to
+//   it. This function will be called for every form element in a loop.
+// - postRequestFunc is a function that will be called if the request completes
+//   successfully, regardless of its status. It takes no parameters.
+//
+// Additional states will be added based on the result of the request.
+// `success` will be set to true if the request was successful. If there is an
+// error, `error` will be set to the error message text, whether that be
+// internally to javascript or the body of the HTTP request on a non-200
+// status.
+export async function submitForm(
+  name,
+  route,
+  stateTemplate,
+  addlProps,
+  setState,
+  transformFunc,
+  postRequestFunc
+) {
+  let obj = {};
+  try {
+    let form = document.getElementById(name);
+
+    for (var i = 0; i < form.length; i++) {
+      obj = transformFunc(form.elements[i], obj);
+    }
+
+    let response = await fetch(route, {
+      method: "POST",
+      body: JSON.stringify(obj),
+    });
+
+    if (response.status !== 200) {
+      setState(
+        assignProperties(
+          stateTemplate,
+          assignProperties(addlProps, {
+            error: await new Response(response.body).text(),
+          })
+        )
+      );
+    } else {
+      setState(
+        assignProperties(
+          stateTemplate,
+          assignProperties(addlProps, {
+            success: true,
+          })
+        )
+      );
+    }
+
+    postRequestFunc();
+  } catch (error) {
+    setState(
+      assignProperties(
+        stateTemplate,
+        assignProperties(addlProps, {
+          error: error.message,
+        })
+      )
+    );
+  }
+}
